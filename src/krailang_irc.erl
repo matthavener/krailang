4d1db111fbd1a0e6c63b67d13ea6207a200e374a
Matt Havener <matthavener@gmail.com>
2014-07-17 22:35:28 -0500

    ping timeout

--- src/krailang_irc.erl
+++ src/krailang_irc.erl
@@ -10,18 +10,28 @@
         terminate/2,
         code_change/3]).
 
--record(state, {parent, sock, chan}).
+-record(state, {parent, sock, chan, timer, args}). % args is [Parent, Server, Port, User, Chan]
 
 start_link(Args) ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
 
-init([Parent, Server, Port, User, Chan] = Args) ->
+init([Parent, _Server, _Port, _User, Chan] = Args) -> 
     io:format("starting irc ~p~n", [Args]),
+    {ok, Sock} = connect(Args),
+    {ok, Timer} = timer:apply_after(1000*60*5, gen_server, call, [self(), reconnect]),
+    {ok, #state{parent=Parent, sock=Sock, timer=Timer, chan=list_to_binary(Chan), args=Args}}.
+
+connect([_Parent, Server, Port, User, _Chan]) ->
     {ok, Sock} = gen_tcp:connect(Server, Port, [binary, {packet, line}]),
     send(Sock, user, User ++ " * *", "lul"),
     send(Sock, nick, User),
     %% chan in binary for matching PRIVMSG
-    {ok, #state{parent=Parent, sock=Sock, chan=list_to_binary(Chan)}}.
+    {ok, Sock}.
+
+handle_call(reconnect, _From, #state{sock=OldSock, args=Args} = State) ->
+    gen_tcp:close(OldSock),
+    {ok, Sock} = connect(Args),
+    {ok, State#state{sock=Sock}};
 
 handle_call(_Request, _From, State) ->
     {reply, ignored, State}.
@@ -29,29 +39,32 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-handle_info(Info, #state{parent=Parent, sock=Sock, chan=Chan} = State) ->
-    case Info of 
-        {irc, say, Txt} ->
-            send(Sock, privmsg, Chan, binary:replace(list_to_binary(Txt), [<<"\r">>,<<"\n">>],<<" ">>, [global]));
-        {tcp, Sock, Data} -> 
-%            io:format("got ~p~n", [Data]),
-            Line = parse_line(Data),
-%            io:format("parsed ~p~n", [Line]),
-            case Line of  
-                {<<"PING">>, _, Token, _, _} ->
-                    send(Sock, pong, "", Token);
-                {<<"376">>, _, _, _, _} ->
-                    send(Sock, join, Chan);
-                {<<"PRIVMSG">>, [Chan], [Msg], User, _} ->
-                    Parent ! {irc, self(), binary_to_list(User), binary_to_list(Msg) };
-                _ ->
-                    ok
-            end;
-        Other ->
-            io:format("weird ~p ~p ~n", [Other, State]),
-            exit([Other, State])
+handle_info({irc, say, Txt}, #state{sock=Sock, chan=Chan} = State) ->
+    send(Sock, privmsg, Chan, binary:replace(list_to_binary(Txt), [<<"\r">>,<<"\n">>],<<" ">>, [global])),
+    {noreply, State};
+handle_info({tcp, Sock, Data}, #state{parent=Parent, sock=Sock, chan=Chan, timer=Timer} = State) ->
+%   io:format("got ~p~n", [Data]),
+    Line = parse_line(Data),
+%   io:format("parsed ~p~n", [Line]),
+    NewState = case Line of  
+        {<<"PING">>, _, Token, _, _} ->
+            send(Sock, pong, "", Token),
+            {ok, cancel} = timer:cancel(Timer),
+            {ok, NewTimer} = timer:apply_after(1000*60*5, gen_server, call, [self(), reconnect]),
+            State#state{timer=NewTimer};
+        {<<"376">>, _, _, _, _} ->
+            send(Sock, join, Chan),
+            State;
+        {<<"PRIVMSG">>, [Chan], [Msg], User, _} ->
+            Parent ! {irc, self(), binary_to_list(User), binary_to_list(Msg) },
+            State;
+        _ ->
+            State
     end,
-    {noreply, State}.
+    {noreply, NewState};
+handle_info(Other, State) ->
+    io:format("weird ~p ~p ~n", [Other, State]),
+    {stop, badarg, State}.
 
 terminate(_Reason, _State) ->
     ok.
